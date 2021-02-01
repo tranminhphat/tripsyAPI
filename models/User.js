ab5a1e86f13f684796dcb7c13bc6ed140ed69f2b
Phat Tran <62209890+miph2004@users.noreply.github.com>
2021-02-01 23:37:24 +0700

    hash user's password before saved to database

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -22,6 +23,13 @@ const userSchema = new mongoose.Schema({
     required: true,
     minlength: [6, "Minimum password length is 6 characters"],
   },
+});
+
+/* Hash user password before saved to database */
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 const User = mongoose.model("user", userSchema);
