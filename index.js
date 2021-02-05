4ab87276090a2660eb09a486600d8ee22b6a1a4b
Phat Tran <62209890+miph2004@users.noreply.github.com>
2021-02-05 17:48:58 +0700

    add cors

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const mongoose = require("mongoose");
 
 const config = require("./config");
 const routes = require("./routes");
 
+const whitelist = config.corsWhiteList;
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
 //Middlewares
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors(corsOptions));
 app.use("/api", routes);
 
 mongoose
