3043f99e8e43b7cb199a8d6375fd8673a9214eee
Phat Tran <62209890+miph2004@users.noreply.github.com>
2021-02-06 23:32:03 +0700

    update status code

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -9,14 +9,14 @@ const requireAuth = (req, res, next) => {
     jwt.verify(token, "tripsy@2021", (err, decodedToken) => {
       if (err) {
         console.log(err.message);
-        res.status(302).send("redirect to login page");
+        res.status(200).send("redirect to login page");
       } else {
         console.log(decodedToken);
         next();
       }
     });
   } else {
-    res.status(302).send("redirect to login page");
+    res.status(200).send("redirect to login page");
   }
 };
 
