965b4e0150e8093d9f7d83257eb99931a2f8a436
Phat Tran <62209890+miph2004@users.noreply.github.com>
2021-03-27 19:22:39 +0700

    update payment controller

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,31 +1,41 @@
 const stripe = require("stripe")(
   "sk_test_51IXjZvDcrQRGXIG6bRiF2kdXHW7FLUjXAQ8tjMSxuu6QIC8Izi1yxTcVI9MJk3kp45crg80hn8IqbfTreXcSMyLN0014iZUAnk"
 );
-const YOUR_DOMAIN = "http://localhost:3000";
+const YOUR_DOMAIN = "http://localhost:3000/experience/";
 
-/* Controller for POST: /api/payment/create-booking-session */
+/* Controller for GET: api/payment/booking/:id */
+exports.getBookingSession = async (req, res) => {
+  const { id } = req.params;
+  const session = await stripe.checkout.sessions.retrieve(id);
+  if (session) {
+    return res.status(200).send(session.payment_status);
+  }
+};
+
+/* Controller for POST: /api/payment/booking */
 exports.createBookingSession = async (req, res) => {
-  const { metadata } = req.body;
+  const { experience, customer } = req.body.metadata;
 
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     mode: "payment",
-    customer_email: metadata.customerEmail,
+    client_reference_id: customer.customerId,
+    customer_email: customer.customerEmail,
     line_items: [
       {
         price_data: {
           currency: "vnd",
-          unit_amount: metadata.price,
+          unit_amount: experience.price,
           product_data: {
-            name: metadata.name,
-            description: metadata.description,
-            images: [metadata.image[0].url],
+            name: experience.name,
+            description: experience.description,
+            images: [experience.image[0].url],
           },
         },
         quantity: 1,
       },
     ],
-    success_url: `${YOUR_DOMAIN}`,
+    success_url: `${YOUR_DOMAIN}${experience.id}/confirm-booking/success?session_id={CHECKOUT_SESSION_ID}`,
     cancel_url: `${YOUR_DOMAIN}?canceled=true`,
   });
 
--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -3,5 +3,6 @@ const router = Router();
 const paymentController = require("../controllers/paymentController");
 
 router.post("/booking", paymentController.createBookingSession);
+router.get("/booking/:id", paymentController.getBookingSession);
 
 module.exports = router;
