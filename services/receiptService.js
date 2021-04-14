570f59dd440c97da1a1cd68931f080d1481486fa
Phat Tran <62209890+miph2004@users.noreply.github.com>
2021-04-14 23:00:33 +0700

    update receipt service

--- services/receiptService.js
+++ services/receiptService.js
@@ -1,7 +1,37 @@
 const Receipt = require("../models/Receipt");
 
 exports.getReceipts = async (filterObj, sortObj) => {
-  return await Receipt.aggregate([{ $match: filterObj }, { $sort: sortObj }]);
+  return await Receipt.aggregate([
+    { $match: filterObj },
+    {
+      $lookup: {
+        from: "experiences",
+        localField: "experienceId",
+        foreignField: "_id",
+        as: "experience",
+      },
+    },
+    { $unwind: "$experience" },
+    {
+      $lookup: {
+        from: "activities",
+        localField: "activityId",
+        foreignField: "_id",
+        as: "activity",
+      },
+    },
+    { $unwind: "$activity" },
+    {
+      $lookup: {
+        from: "users",
+        localField: "hostId",
+        foreignField: "_id",
+        as: "host",
+      },
+    },
+    { $unwind: "$host" },
+    { $sort: sortObj },
+  ]);
 };
 
 exports.createReceipt = async (model) => {
