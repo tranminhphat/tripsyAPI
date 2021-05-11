bb0fa9bde2fc979a9dafae7a25adfaa66ffcbecd
Phat Tran <62209890+miph2004@users.noreply.github.com>
2021-05-11 22:18:10 +0700

    update experience route

--- routes/experienceRoutes.js
+++ routes/experienceRoutes.js
@@ -7,6 +7,10 @@ router.get("/", experienceController.getExperiences);
 router.get("/date/:dayOfYear", experienceController.getExperiencesByDate);
 router.get("/:id", experienceController.getExperienceById);
 router.get("/similar/:id", experienceController.getSimilarExperience);
+router.get(
+  "/recommend/:id",
+  experienceController.getRecommendExperienceForUser
+);
 router.post("/", requireAuth, experienceController.createExperience);
 router.put("/:id", requireAuth, experienceController.updateExperienceById);
 router.put(
