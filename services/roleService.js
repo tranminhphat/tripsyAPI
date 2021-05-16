ebe5e260b62f1c5230bc5b27d47dc8137d9bccb6
Phat Tran <62209890+miph2004@users.noreply.github.com>
2021-05-16 19:35:18 +0700

    update role service

--- services/roleService.js
+++ services/roleService.js
@@ -1,5 +1,9 @@
 const Role = require("../models/Role");
 
+exports.getRoles = async () => {
+  return await Role.find();
+};
+
 exports.getRoleByRoleName = async (roleName) => {
   return await Role.findOne({ roleName }).exec();
 };
